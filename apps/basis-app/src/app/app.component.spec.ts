ba1db0ec0dc0df61b81875fc48797b8b8c1316fa
webia1 <webia1.github@gmail.com>
2023-09-05 17:02:51 +0200

    Updating tests

--- apps/basis-app/src/app/app.component.spec.ts
+++ apps/basis-app/src/app/app.component.spec.ts
@@ -1,27 +1,33 @@
 import { TestBed } from '@angular/core/testing';
 import { AppComponent } from './app.component';
-import { NxWelcomeComponent } from './nx-welcome.component';
 import { RouterTestingModule } from '@angular/router/testing';
 
 describe('AppComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [AppComponent, NxWelcomeComponent, RouterTestingModule],
+      imports: [AppComponent,  RouterTestingModule],
     }).compileComponents();
   });
 
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain(
-      'Welcome angular-basis-app'
-    );
-  });
+  /**
+   * App Component has no template, only a router-outlet.
+   * For demonstration purposes:
+   *  - we test the only the title here
+   *  - render part is commented out
+   */
+
+  // it('should render title', () => {
+  //   const fixture = TestBed.createComponent(AppComponent);
+  //   fixture.detectChanges();
+  //   const compiled = fixture.nativeElement as HTMLElement;
+  //   expect(compiled.querySelector('h1')?.textContent).toContain(
+  //     'Angular v16 Basis App'
+  //   );
+  // });
 
   it(`should have as title 'angular-basis-app'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('angular-basis-app');
+    expect(app.title).toEqual('Angular v16 Basis App');
   });
 });
