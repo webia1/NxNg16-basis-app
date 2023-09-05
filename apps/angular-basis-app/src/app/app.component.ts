78a41f4e98b3b9961f04c521285f919c0daaf49d
webia1 <webia1.github@gmail.com>
2023-09-05 11:04:25 +0200

    Noise (Unnecessary Files & Templates) removed

--- apps/angular-basis-app/src/app/app.component.ts
+++ apps/angular-basis-app/src/app/app.component.ts
@@ -1,14 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
-import { NxWelcomeComponent } from './nx-welcome.component';
 
 @Component({
   standalone: true,
-  imports: [NxWelcomeComponent, RouterModule],
+  imports: [RouterModule],
   selector: 'angular-basis-app-workspace-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'angular-basis-app';
+  title = 'Angular v16 Basis App';
 }
