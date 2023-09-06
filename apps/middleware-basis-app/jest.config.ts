6509f48297978ea36783d97b89e47fc5cbd3cd9f
webia1 <webia1.github@gmail.com>
2023-09-06 16:18:39 +0200

    NestJS Integration

---
+++ apps/middleware-basis-app/jest.config.ts
@@ -0,0 +1,14 @@
+/* eslint-disable */
+export default {
+  displayName: 'middleware-basis-app',
+  preset: '../../jest.preset.js',
+  testEnvironment: 'node',
+  transform: {
+    '^.+\\.[tj]s$': [
+      'ts-jest',
+      { tsconfig: '<rootDir>/tsconfig.spec.json' },
+    ],
+  },
+  moduleFileExtensions: ['ts', 'js', 'html'],
+  coverageDirectory: '../../coverage/apps/middleware-basis-app',
+};
